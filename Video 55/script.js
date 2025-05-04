9dd72b99dfc9888eac90f50cdcc5091434243bf6
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-05-05 04:56:27 +0530

    JavaScript Concepts

---
+++ Video 55/script.js
@@ -0,0 +1,45 @@
+console.log("This is tutorial for JS")
+
+var a = 5;
+a = a + 1;
+var b = 6;
+var c = "Zander";
+var _a = "Shubham";
+// var 55a = "Rohan";    Invalid
+
+
+{
+    let a = 66;
+    // let has a block scope and var has global scope
+    console.log(a)
+}
+
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)
+
+const a1 = 6;
+// a1 = a1 + 1;    Invalid
+
+console.log(a1)
+
+let x = "Test String";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Z",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o)
+
+o.salary = "100 rupees"
+
+console.log(o)
