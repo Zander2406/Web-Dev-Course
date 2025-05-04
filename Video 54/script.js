153bf93a045b7c303f164a9ba9c213c1ed122063
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-05-04 12:17:55 +0530

    Introduction to JavaScript

---
+++ Video 54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code is running...")
+
+var a = prompt("Enter your number")
+var isTrue = prompt("Are you sure you want to leave and burn your computer")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else {
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+
+// document.body.style.backgroundColor = "red"
