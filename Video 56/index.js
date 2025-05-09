4301ba0f2b79125a457ee16199cbaff6f57aeded
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-05-09 13:29:29 +0530

    Simple concepts in JavaScript

---
+++ Video 56/index.js
@@ -0,0 +1,25 @@
+console.log("Conditional Tutorial")
+
+let age = 1;
+// let grace = 2;
+// age += grace;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+if(age == 18){
+    console.log("You can drive");
+}
+else if(age == 0){
+    console.log("Are you kidding?")
+}
+else if(age == 1){
+    console.log("Are you again kidding?")
+}
+else {
+    console.log("You cannot drive");
+}
