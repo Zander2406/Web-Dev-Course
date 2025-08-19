11416a7cdbeabc813d58aa79f87d5cee3065f9dd
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-08-19 14:54:17 +0530

    Spotify Clone Adding songs from directory

---
+++ Video 84/Spotify_Clone/script.js
@@ -0,0 +1,43 @@
+console.log("Let's write JavaScript");
+
+
+async function getSongs(){
+    
+    let a = await fetch("http://192.168.0.147:3000/songs/");
+    let response = await a.text();
+    console.log(response);
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    as = div.getElementsByTagName("a");
+    let songs = [];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1]);
+        }
+    }
+    return songs;
+}
+
+async function main(){   
+    // Get the list of all songs
+    let songs = await getSongs();
+    console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+    }
+
+    // Play the first song
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", ()=> {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    })
+}
+
+main();
+
