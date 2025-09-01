bfb4249e1a77197637765ac079afb20ef6b253b7
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-09-01 22:47:12 +0530

    Nodejs fs and path module

---
+++ Video 87/aboutpath.js
@@ -0,0 +1,11 @@
+import path from "path"
+
+
+let myPath = "C:\\Users\\Swarnava\\Desktop\\Sigma Web Development Course\\Web-Dev-Course\\Video 87\\harry.txt"
+
+console.log(path.extname(myPath));
+
+console.log(path.dirname(myPath));
+console.log(path.basename(myPath));
+
+console.log(path.join("c:/", "programs\\harry.txt"));
