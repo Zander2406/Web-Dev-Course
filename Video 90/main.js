b67eeadc09c41e89e9ae036c6fa64a49478fd30b
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-09-04 15:34:27 +0530

    Middlewares explained

---
+++ Video 90/main.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const blog = require('./routes/blog')
+const fs = require('fs');
+
+// app.use(express.static('public'))
+
+app.use('/blog', blog)
+
+// Middlewarre 1 - Logger for our application
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.harry = 'I am harry bhai'
+    fs.appendFileSync("logs.txt", `${Date.now()} is a ${req.method}\n`)
+    console.log(`${Date.now()} is a ${req.method}`)
+    // res.send("Hacked by Middleware 1")
+    next()
+})
+
+// Middleware 2
+app.use((req, res, next) => {
+    console.log('m2')
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hello about!' + req.harry)
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
