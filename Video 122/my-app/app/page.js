7a7faf2621629638f1b644ecf4d4f26cf64556ee
Swarnava Bhattacharya <swarnavabhattacharya24@gmail.com>
2025-12-02 16:24:38 +0530

    Client and Server Components Explained

---
+++ Video 122/my-app/app/page.js
@@ -0,0 +1,20 @@
+// "use client"
+// import { useState, useEffect } from "react";
+import fs from "fs/promises"
+import Navbar from "@/components/Navbar"
+
+export default function Home() {
+  // const [count, setCount] = useState(0)
+  console.log("Hey I am harry")
+  let a = fs.readFile(".gitignore")
+  a.then(e=>{console.log(e.toString())})
+
+  return (
+    <div>
+      <Navbar />
+      {/* I am a component {count} */}
+      I am a component
+      {/* <button onClick={() => setCount(count + 1)}> Click Me </button> */}
+    </div>
+  );
+}
